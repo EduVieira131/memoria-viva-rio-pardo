a381b91584733c6d314328dbcc4bcc18d3ae6658
EduViera131 <eduviera369@gmail.com>
2024-06-21 09:54:02 -0300

    Initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className={`min-h-screen mx-auto max-w-xl px-8 lg:px-16 font-body`}>
         <Main />
         <NextScript />
       </body>
