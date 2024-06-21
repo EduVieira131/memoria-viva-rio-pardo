3001ef85380951bbdd114286095ecabb8b34a5aa
EduViera131 <eduviera369@gmail.com>
2024-06-21 16:46:11 -0300

    :feat: Create history of the city section

---
+++ src/pages/history.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function History() {
+  return (
+    <div>
+      <h1> Oiiiii</h1>
+
+      <Link href={"/"}>Voltar</Link>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,6 +140,34 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        <section className="flex items-center justify-between w-full h-[500px] bg-sand-100 p-6">
+          <Image
+            src={"/historic-build.png"}
+            width={400}
+            height={350}
+            alt="Imagem da catedral da cidade"
+            className="w-[40%]"
+          />
+
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-3 max-w-[500px]">
+              <h1 className="font-display text-3xl text-green-800 leading-[135%] font-bold">
+                Um prédio restaurado, uma história renovada
+              </h1>
+              <p className="text-base text-pretty text-green-800">
+                Cada prédio possui um marco na história, contando fatos de sua
+                época ou representando o dia a dia das pessoas que ali viveram.
+              </p>
+            </div>
+
+            <div className="space-x-3">
+              <Button asChild>
+                <Link href={"/history"}>Ler história de Rio Pardo</Link>
+              </Button>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
