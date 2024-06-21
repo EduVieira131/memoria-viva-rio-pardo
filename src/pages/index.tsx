d76709a7d709175cd724c11fcefac3836daf2de9
EduViera131 <eduviera369@gmail.com>
2024-06-21 14:32:01 -0300

    :feat: Create feedback section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
+import Avatar from "../../public/avatar.svg";
+
 export default function Home() {
   return (
     <>
@@ -16,7 +18,7 @@ export default function Home() {
         >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-3 max-w-[500px]">
-              <h1 className="font-display text-3xl text-green-800 leading-[135%]">
+              <h1 className="font-display text-3xl text-green-800 leading-[135%] font-bold">
                 Descubra pontos históricos restaurados
               </h1>
               <p className="text-base text-pretty text-green-800">
@@ -46,6 +48,27 @@ export default function Home() {
             alt="Imagem da catedral da cidade"
             className="w-[40%]"
           />
+        </section>
+
+        <section className="mx-auto my-[100px] w-[780px] flex flex-col gap-10">
+          <h2 className="font-display leading-[135%] text-2xl text-green-800 font-bold">
+            “Uma das melhores experiências que já tive. Locais magníficos,
+            passeios interessantes e equipe atenciosa!”
+          </h2>
+
+          <div className="flex items-center gap-4">
+            <Image
+              src={Avatar}
+              alt="Avatar de um homem de barba e chapéu"
+              width={70}
+              height={70}
+            />
+
+            <div className="flex flex-col gap-1">
+              <span className="text-sm text-green-600">João Silva</span>
+              <span className="text-sm text-green-600">Empresário</span>
+            </div>
+          </div>
         </section>
       </main>
     </>
